af7c2a52379e8bc5db2486063239e74c79123450
Vince Weaver <vincent.weaver@maine.edu>
2015-09-11 11:17:24 -0400

    fix some indentation and compile warnings

--- vmw_glfont.c
+++ vmw_glfont.c
@@ -5,7 +5,7 @@
 
 #include <stdio.h>  /* For File I/O */
 #include <stdlib.h> /* For malloc/calloc */
-
+#include <string.h> /* For strlen() */
 
 /* The font used is a standard VGA font.          */
 /* To use the "smallText" functions, the VGA font */
@@ -24,23 +24,22 @@ GLuint words_texture;
 static int font_scale,font_xsize,font_ysize;
 
 void double_size(GLubyte value) {
-   
-    int i; 
-   
-    high=0; low=0;
-   
-    for(i=0;i<4;i++) {
-       if ((value>>i)&1) {
-	  low|=3<<(i*2);
-       }
-    }
-   
-    for(i=0;i<4;i++) {
-       if ((value>>(4+i))&1) {
-	  high|=3<<(i*2);
-       }
-    }
-	  
+
+	int i;
+
+	high=0; low=0;
+
+	for(i=0;i<4;i++) {
+		if ((value>>i)&1) {
+			low|=3<<(i*2);
+		}
+	}
+
+	for(i=0;i<4;i++) {
+		if ((value>>(4+i))&1) {
+			high|=3<<(i*2);
+		}
+	}
 }
 
 
@@ -266,119 +265,114 @@ int texture_outtext(char *string,GLuint texture) {
    
 }
 #endif
+
 int setup_texture_font(char *filename) {
-   
-    static GLubyte texture_data[128][128][4];
 
-    unsigned char buff[16];
-    FILE *f;
-    int i,j,row,col;
-   
-    f=fopen(filename,"r");
-    if (f==NULL) {
-       printf("ERROR loading font file %s.\n",filename);
-       return -1;
-    }
+	static GLubyte texture_data[128][128][4];
 
-    for(i=0;i<128;i++) for (j=0;j<128;j++) {
-       texture_data[i][j][0]=0;
-       texture_data[i][j][1]=0;
-       texture_data[i][j][2]=0;
-       texture_data[i][j][3]=255;
-    }
-       
-   
-    for(row=0;row<12;row++) {
-       for(col=0;col<12;col++) {
-	  fread(buff,1,16,f);
-	  for(j=0;j<8;j++) {
-	     for(i=7;i>=0;i--) {
+	unsigned char buff[16];
+	FILE *f;
+	int i,j,row,col;
+
+	f=fopen(filename,"r");
+	if (f==NULL) {
+		printf("ERROR loading font file %s.\n",filename);
+		return -1;
+	}
+
+	for(i=0;i<128;i++) for (j=0;j<128;j++) {
+		texture_data[i][j][0]=0;
+		texture_data[i][j][1]=0;
+		texture_data[i][j][2]=0;
+		texture_data[i][j][3]=255;
+	}
+
+	for(row=0;row<12;row++) {
+		for(col=0;col<12;col++) {
+			fread(buff,1,16,f);
+				for(j=0;j<8;j++) {
+					for(i=7;i>=0;i--) {
 		if (buff[j]&(1<<i)) {
-		   texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [0] =255;
-		   texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [1] =255;
-		   texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [2] =255;
-		   texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [3] =255;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [0] =255;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [1] =255;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [2] =255;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [3] =255;
 		}
 		else {
-		    texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [0] =0;
-		    texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [1] =0;
-		    texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [2] =0;
-		    texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [3] =255;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [0] =0;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [1] =0;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [2] =0;
+			texture_data[ (col*9)+(7-i) ] [ (row*9)+j ] [3] =255;
+					}
+				}
+			}
 		}
-	     }
-	  }
-       }
-    }
-    
-    fclose(f);
+	}
+
+	fclose(f);
 
 #ifdef BIG_ENDIAN
-    glPixelStorei(GL_UNPACK_SWAP_BYTES,1);
+	glPixelStorei(GL_UNPACK_SWAP_BYTES,1);
 #endif
-   
-    glPixelStorei(GL_UNPACK_ALIGNMENT,1);
-    glGenTextures(1,&font_texture);
-    glBindTexture(GL_TEXTURE_2D,font_texture);
-   
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 128,
-		   128 ,0,GL_RGBA,GL_UNSIGNED_BYTE,texture_data);
 
-   
+	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+	glGenTextures(1,&font_texture);
+	glBindTexture(GL_TEXTURE_2D,font_texture);
 
-   
-    for(j=0;j<12;j++) {
-       for(i=0;i<12;i++) {
-	  texture_font_list[j*12+i]=glGenLists(1);
-	  glNewList(texture_font_list[j*12+i],GL_COMPILE);
-	  glEnable(GL_TEXTURE_2D);  
-	  glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_REPLACE);
-          glBindTexture(GL_TEXTURE_2D,font_texture);
-	  glBegin(GL_QUADS);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 128,
+		128 ,0,GL_RGBA,GL_UNSIGNED_BYTE,texture_data);
 
-	    glNormal3f(0,0,1);
-	    glTexCoord2f((j*9+9)/128.0,(i*9)/128.0);
-	    glVertex3f(-0.5,0,-0.5);
-	  glTexCoord2f( (j*9)/128.0 ,(i*9)/128.0);
-	    glVertex3f(-0.5,0,0.5);
-	  glTexCoord2f( (j*9)/128.0,(i*9+9)/128.0);
-	    glVertex3f(0.5,0,0.5);
-	  glTexCoord2f( (j*9+9)/128.0,(i*9+9)/128.0);
-	    glVertex3f(0.5,0,-0.5);
+	for(j=0;j<12;j++) {
+		for(i=0;i<12;i++) {
+			texture_font_list[j*12+i]=glGenLists(1);
+			glNewList(texture_font_list[j*12+i],GL_COMPILE);
+			glEnable(GL_TEXTURE_2D);
+			glTexEnvf(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_REPLACE);
+			glBindTexture(GL_TEXTURE_2D,font_texture);
+			glBegin(GL_QUADS);
+
+			glNormal3f(0,0,1);
+			glTexCoord2f((j*9+9)/128.0,(i*9)/128.0);
+			glVertex3f(-0.5,0,-0.5);
+			glTexCoord2f( (j*9)/128.0 ,(i*9)/128.0);
+			glVertex3f(-0.5,0,0.5);
+			glTexCoord2f( (j*9)/128.0,(i*9+9)/128.0);
+			glVertex3f(0.5,0,0.5);
+			glTexCoord2f( (j*9+9)/128.0,(i*9+9)/128.0);
+			glVertex3f(0.5,0,-0.5);
 #if 0
-	  	    glNormal3f(0,0,1);
-	    glTexCoord2i((j*9+9),(i*9));
-	    glVertex3f(-0.5,0,-0.5);
-	  glTexCoord2i( (j*9) ,(i*9));
-	    glVertex3f(-0.5,0,0.5);
-	  glTexCoord2i( (j*9),(i*9+9));
-	    glVertex3f(0.5,0,0.5);
-	  glTexCoord2i( (j*9+9),(i*9+9));
-	    glVertex3f(0.5,0,-0.5);
-#endif	  
-	  glDisable(GL_TEXTURE_2D);  
-	  
-	  glEnd();
-	  glEndList();
-	  
-       }
-       
-    }
-   
+			glNormal3f(0,0,1);
+			glTexCoord2i((j*9+9),(i*9));
+			glVertex3f(-0.5,0,-0.5);
+			glTexCoord2i( (j*9) ,(i*9));
+			glVertex3f(-0.5,0,0.5);
+			glTexCoord2i( (j*9),(i*9+9));
+			glVertex3f(0.5,0,0.5);
+			glTexCoord2i( (j*9+9),(i*9+9));
+			glVertex3f(0.5,0,-0.5);
+#endif
+			glDisable(GL_TEXTURE_2D);
+			glEnd();
+			glEndList();
+		}
+	}
+	return 0;
+
 }
 
 int texture_put_string(char *string) {
-   
-   int i;
-   
-   for(i=0;i<strlen(string);i++) {
-      glCallList(texture_font_list[(int)string[i]]);          
-      glTranslatef(1.0,0,0);
 
-   }
+	int i;
+
+	for(i=0;i<strlen(string);i++) {
+		glCallList(texture_font_list[(int)string[i]]);
+		glTranslatef(1.0,0,0);
+	}
+	return 0;
 }
 
 
